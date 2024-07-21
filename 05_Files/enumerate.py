419115e91c4d1637ee42a92b5fc57a64292496d1
GaneshJavas <rjaiganesh07@gmail.com>
2024-07-21 14:47:44 +0530

    enum

--- 05_Files/enumerate.py
+++ 05_Files/enumerate.py
@@ -2,15 +2,40 @@
 
 # enumerate
 
-my_list = ("amit", "satyam", "gulshan", "vipin")
+my_list = [{"name": "Amit", "age": "18"},{"name": "Vipin", "age": "19"}]
 
 enum_list = enumerate(my_list)
 
-print(enum_list)
+# print(enum_list)
 
-print(list(enum_list))
+# print(list(enum_list))
 
 
+
+# for i in enum_list:
+#     print(i)
+
+# Output:
+# (0, {'name': 'Amit', 'age': '18'})
+# (1, {'name': 'Vipin', 'age': '19'})
+
+# for i in enum_list:
+#     print(i[0])
+
+# Output:
+# 0
+# 1
+
+# for i in enum_list:
+#     print(i[1])
+
+# Output:
+# {'name': 'Amit', 'age': '18'}
+# {'name': 'Vipin', 'age': '19'}
 
 for i in enum_list:
-    print(i)
+    print(f"{i[1]['name']}")
+
+# Output:
+# Amit
+# Vipin
